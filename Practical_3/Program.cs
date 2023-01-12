854bdfd0d259918b17b8fab4dd13ebac26840737
denkbr <den.kbr@mail.ru>
2023-01-12 19:09:35 +0300

    Практическая 3. Задача 21.

--- Practical_3/Program.cs
+++ Practical_3/Program.cs
@@ -1,2 +1,50 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними в 3D пространстве.
+
+// A (3,6,8); B (2,1,-7), -> 15.84
+
+// A (7,-5, 0); B (1,-1,9) -> 11.53
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        char selection;
+        Double x1, x2, y1, y2, z1, z2, distance;
+
+        Console.WriteLine("Расстояние между двумя точками в пространстве. Для запуска введите 1");   
+
+        selection = Convert.ToChar(Console.ReadLine());
+
+        switch (selection)
+        {
+            case '1':
+                Console.Write("Введите координаты x1: ");
+                x1 = Convert.ToDouble(Console.ReadLine());
+
+                Console.Write("Введите координаты x2: ");
+                x2 = Convert.ToDouble(Console.ReadLine());
+
+                Console.Write("Введите координаты y1: ");
+                y1 = Convert.ToDouble(Console.ReadLine());
+
+                Console.Write("Введите координаты y2: ");
+                y2 = Convert.ToDouble(Console.ReadLine());
+
+                Console.Write("Введите координаты z1: ");
+                z1 = Convert.ToDouble(Console.ReadLine());
+
+                Console.Write("Введите координаты z2: ");
+                z2 = Convert.ToDouble(Console.ReadLine());
+
+                distance = Math.Sqrt(Math.Pow(x2 - x1, 2) + Math.Pow(y2 - y1, 2) + Math.Pow(z2 - z1, 2));
+                Console.WriteLine($"Расстояние между двумя точками в пространстве равно {distance}");
+                Console.ReadKey();
+                break;
+            default:
+                Console.WriteLine("Вы ввели неверный символ");
+                Console.ReadKey();
+                break;
+        }
+
+    }
+}
