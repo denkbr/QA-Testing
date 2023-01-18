d12930f42698e3c9878f113a1b3a0df12476dcae
denkbr <den.kbr@mail.ru>
2023-01-18 18:26:01 +0300

    zadacha34

--- Practical_5/Program.cs
+++ Practical_5/Program.cs
@@ -1,2 +1,23 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 34: Задайте массив заполненный случайными положительными трёхзначными числами. Напишите программу, которая покажет количество чётных чисел в массиве.
+
+// [345, 897, 568, 234] -> 2
+
+Console.Write("Введите количество элементов массива: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+int[] array = new int[num];
+
+int count = 0;
+for (int i = 0; i < array.Length; i++)
+{
+    array[i] = new Random().Next(100, 1000);
+}
+
+for (int i = 0; i < array.Length; i++)
+{
+        Console.Write($"{array[i]} ");
+
+            if (array[i] % 2 == 0) count++;
+}
+Console.WriteLine();
+System.Console.WriteLine($"Количество чётных элементов массива: {count}");
