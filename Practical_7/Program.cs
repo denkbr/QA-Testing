5389a22f6b3ae3eb630e87f27a08b451a5a0f480
denkbr <den.kbr@mail.ru>
2023-01-22 11:52:48 +0300

    zadacha50

--- Practical_7/Program.cs
+++ Practical_7/Program.cs
@@ -1,25 +1,52 @@
-﻿
+﻿// Задача 50. Напишите программу, которая на вход принимает позиции элемента в двумерном массиве, и возвращает значение этого элемента или же указание, что такого элемента нет.
 
-int n = InputInt("Введите положительное число ");
-int m = 1;
-while (n < 1)
+// Например, задан массив:
+
+// 1 4 7 2
+
+// 5 9 2 3
+
+// 8 4 2 4
+
+Console.WriteLine("введите номер строки");
+int n = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("введите номер столбца");
+int m = Convert.ToInt32(Console.ReadLine());
+int [,] numbers = new int [10,10];
+FillArrayRandomNumbers(numbers);
+
+if (n > numbers.GetLength(0) || m > numbers.GetLength(1))
 {
-    Console.WriteLine("Ввели не положительное число! Введите положительное число.");
-    break;
+    Console.WriteLine("такого элемента нет");
 }
-Console.WriteLine(NaturalNumber(n, m));
-
-int NaturalNumber(int n, int m)
+else
 {
-    if (n == m)
-        return n;
-    else
-        Console.Write($"{NaturalNumber(n, m + 1)}, ");
-    return m;
+    Console.WriteLine($"значение элемента {n} строки и {m} столбца равно {numbers[n-1,m-1]}");
 }
 
-int InputInt(string output)
+PrintArray(numbers);
+
+void FillArrayRandomNumbers(int[,] array)
 {
-    Console.Write(output);
-    return int.Parse(Console.ReadLine());
+    for (int i = 0; i < array.GetLength(0); i++)
+        {        
+            for (int j = 0; j < array.GetLength(1); j++)
+            {
+                array [i,j] = new Random().Next(-100, 100)/10;
+            }   
+        }
+}
+
+void PrintArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        Console.Write("[ ");
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write(array[i,j] + " ");
+        }   
+        Console.Write("]");
+        Console.WriteLine(""); 
+    }
 }
